e096020db2db47c5b53e1df9b435dc1f93876e1d
mmalone <mjmalone@gmail.com>
2010-08-03 15:47:25 -0700

    Add function to generate random token.

--- goldengate/__init__.py
+++ goldengate/__init__.py
@@ -1,6 +1,15 @@
-from goldengate import GoldenGate, application
-
 __version__ = 0.1
 __date__ = '30 July 2010'
 __author__ = 'Mike Malone'
 __credits__ = 'SimpleGeo'
+
+from goldengate import GoldenGate, application
+
+
+RANDOM_TOKEN_STRING_LENGTH = 16
+RANDOM_TOKEN_ALPHABET = 'abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'
+
+
+def random_token(length=RANDOM_TOKEN_STRING_LENGTH, alphabet=RANDOM_TOKEN_ALPHABET):
+    "Generate a random string with the given length and alphabet."
+    return ''.join(random.choice(alphabet) for _ in xrange(length))
